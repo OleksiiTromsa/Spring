f9d2e2fb23a76309d619d8b8834ae97eed1b3be3
Oleksii Tromsa <tromsaalex@gmail.com>
2020-10-21 10:53:24 +0300

    Delete unused dependencies and bean

--- src/main/java/spring/intro/config/WebConfig.java
+++ src/main/java/spring/intro/config/WebConfig.java
@@ -11,12 +11,4 @@ import org.springframework.web.servlet.view.JstlView;
 @EnableWebMvc
 @ComponentScan(basePackages = {"spring.intro.controllers"})
 public class WebConfig {
-    @Bean
-    public InternalResourceViewResolver resolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setViewClass(JstlView.class);
-        resolver.setPrefix("/WEB-INF/views/");
-        resolver.setSuffix(".jsp");
-        return resolver;
-    }
 }
